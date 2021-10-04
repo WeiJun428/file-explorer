47df4446cec777b760aca45ccc7d16c6e43e7bd8
wj428 <wj428@uw.edu>
2021-10-03 17:22:27 -0700

    second commit

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
 /**
- * Name: Wei Jun Tan
- * Date: May 19, 2021
- * Section: CSE 154 AH (TA: Victor Shan)
- * This index.js adds an interactive UI to the site.
- * It controls the interactive buttons for navigation
- * and form submission for web server request.
+ *
  */
 
- "use strict";
+"use strict";
 
- (function() {
-   window.addEventListener("load", init);
+const fs = require("fs").promises;
 
-   /**
-    * Initialize the interative elements once the window is loaded.
-    */
-   function init() {
-     console.log("haha");
-   }
+(function() {
+  window.addEventListener("load", init);
 
-   /**
-    * Returns the element that has the ID attribute with the specified value.
-    * @param {string} idName - element ID
-    * @returns {object} DOM object associated with id.
-    */
-   function id(idName) {
-     return document.getElementById(idName);
-   }
+  /**
+  * Initialize the interative elements once the window is loaded.
+  */
+  async function init() {
+    let file = await fs.readFile("README.md", "utf8");
+    id("console").textContent = "file";
+  }
 
-   /**
-    * Returns the first element that matches the given CSS selector.
-    * @param {string} selector - CSS query selector.
-    * @returns {object} The first DOM object matching the query.
-    */
-   function qs(selector) {
-     return document.querySelector(selector);
-   }
+  /**
+  * Returns the element that has the ID attribute with the specified value.
+  * @param {string} idName - element ID
+  * @returns {object} DOM object associated with id.
+  */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
 
-   /**
-    * Returns a new element with the given tag name.
-    * @param {string} tagName - HTML tag name for new DOM element.
-    * @returns {object} New DOM object for given HTML tag.
-    */
-   function gen(tagName) {
-     return document.createElement(tagName);
-   }
- })();
+  /**
+  * Returns the first element that matches the given CSS selector.
+  * @param {string} selector - CSS query selector.
+  * @returns {object} The first DOM object matching the query.
+  */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+  * Returns a new element with the given tag name.
+  * @param {string} tagName - HTML tag name for new DOM element.
+  * @returns {object} New DOM object for given HTML tag.
+  */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+})();
--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 /**
- * Heading
+ * This is the main process of the app File Explorer.
  */
 
 "use strict";
 
-const { app, BrowserWindow } = require('electron');
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-app.whenReady().then(() => {
-  createWindow();
-})
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-})
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+  }
+});
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 500,
+    // fullscreen: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
-  })
+  });
 
-  win.loadFile('index.html')
-}
+  win.loadFile(path.join(__dirname, "index.html"));
+};
